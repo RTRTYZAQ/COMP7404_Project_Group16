9185ec6fbc70e8671c160b3257193bb3878d7b4b
huiyt123 <u3637084@connect.hku.hk>
2025-03-25 20:18:40 +0800

    sklearn_AP

--- sklearn_AP.py
+++ sklearn_AP.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 from sklearnex import patch_sklearn
 patch_sklearn()
 from sklearn.cluster import AffinityPropagation
@@ -11,7 +12,7 @@ from Visualization.PCA_2d import PCA_2d_with_centers
 
 # 读取TSV文件
 input_file = './Dataset/Olivetti_Faces/olivetti_faces.tsv'  # 输入文件路径
-output_file = './labels.tsv'  # 输出文件路径（得改）
+output_file = './sklearn_AP_output/labels.tsv'  # 输出文件路径
 
 # 读取数据
 X = pd.read_csv(input_file, sep='\t', header=None)
@@ -22,6 +23,8 @@ X_scaled = scaler.fit_transform(X)
 
 # 应用AP聚类（调整preference参数控制聚类数量）
 af = AffinityPropagation(random_state=0, max_iter=1000).fit(X_scaled)
+if input_file == './Dataset/Olivetti_Faces/olivetti_faces.tsv':
+    af = AffinityPropagation(random_state=0, max_iter=1000, preference=-5000, damping = 0.6).fit(X_scaled)
 labels = af.labels_
 cluster_centers_scaled = af.cluster_centers_
 cluster_centers = scaler.inverse_transform(cluster_centers_scaled)  # 转换回原始坐标
@@ -31,7 +34,10 @@ unique_labels, counts = np.unique(labels, return_counts=True)
 for label, count in zip(unique_labels, counts):
     print(f"Cluster {label} has {count} points")
 # 保存标签文件
-pd.DataFrame({'label': labels}).to_csv('labels.tsv', sep='\t', index=False, header=False)
+if not os.path.exists(output_file):
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+pd.DataFrame(labels).to_csv(output_file, sep='\t', index=False, header=False)
+print(f"聚类结果已保存到 {output_file}")
 print(f'cluster_center_indices: {cluster_center_indices}')
 # 单独保存索引文件
 pd.DataFrame({'center_index': cluster_center_indices})\
